3fca400c10087fa87ac2a3378b7571bdbb47aca5
wangbenjun5@163.com <33Za_QLhju>
2017-07-12 22:25:41 +0800

    优化爬取美女图片

--- picScrapy/spiders/pic.py
+++ picScrapy/spiders/pic.py
@@ -1,7 +1,6 @@
 # -*- coding:utf-8 -*-
 # ! /bin/bash/python3
 
-import re
 from urllib.parse import urljoin
 from scrapy.spiders import Spider
 from scrapy.http import Request
@@ -10,7 +9,6 @@ from picScrapy.items import PicscrapyItem
 
 class PicSpider(Spider):
     name = "pic"  # 定义爬虫名
-    start_url = 'http://www.jj20.com'  # 爬虫入口
     headers = {
         'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 '
                       '(KHTML, like Gecko) Chrome/59.0.3071.104 Safari/537.36',
@@ -21,15 +19,25 @@ class PicSpider(Spider):
             url = 'http://www.jj20.com/bz/nxxz/list_7_cc_14_%d.html' % i
             yield Request(url, headers=self.headers)
 
-    # 该函数名不能改变，因为Scrapy源码中默认callback函数的函数名就是parse
+    # 一级页面的处理函数
     def parse(self, response):
+        # 提取界面所有的符合入口条件的url
+        all_urls = response.xpath('//div[@class="main"]/ul/li/a/@href').extract()
+        # 遍历获得的url，继续爬取
+        for url in all_urls:
+            # urljoin生成完整url地址
+            url = urljoin(response.url, url)
+            yield Request(url, callback=self.parse_img)
+
+    # 二级页面的处理函数
+    def parse_img(self, response):
         item = PicscrapyItem()
+        # 提前页面符合条件的图片地址
         item['image_urls'] = response.xpath('//img[@id="bigImg"]/@src').extract()
         yield item
-        # 提取界面所有的url
-        all_urls = response.xpath('//div/ul/li/a/@href').extract()
-        # 遍历获得的url，如果满足条件，继续爬取
+        # 提取界面所有复合条件的url
+        all_urls = response.xpath('//ul[@id="showImg"]/li/a/@href').extract()
+        # 遍历获得的url，继续爬取
         for url in all_urls:
-            if re.search(r'\d{4}.html||\d{4}_\d+.html', url):
-                url = urljoin(response.url, url)
-                yield Request(url, callback=self.parse)
+            url = urljoin(response.url, url)
+            yield Request(url, callback=self.parse_img)
