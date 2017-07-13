f7f61d4ecc29d17c71a8144fce87c22aa1c7cd5e
wangbenjun <wangbenjun@gsfa.com>
2017-07-13 15:55:37 +0800

    整理代码和注释

--- picScrapy/pipelines.py
+++ picScrapy/pipelines.py
@@ -11,16 +11,17 @@ from scrapy import Request
 
 
 class PicscrapyPipeline(ImagesPipeline):
+
     def get_media_requests(self, item, info):
+        # 通过meta属性传递title
         return [Request(x, meta={'title': item['title']}) for x in item.get(self.images_urls_field, [])]
 
-    # 重写函数，修改了下载图片名称的生成规则，改成了url地址的最后一个加了随机数
+    # 重写函数，修改了下载图片名称的生成规则
     def file_path(self, request, response=None, info=None):
         if not isinstance(request, Request):
             url = request
         else:
             url = request.url
         url = urlparse(url)
-        # path_name = url.path.split('/')[5].split('.')[0].split('-')[0]
         img_name = url.path.split('/')[5].split('.')[0]
         return request.meta['title'] + '/%s.jpg' % img_name
